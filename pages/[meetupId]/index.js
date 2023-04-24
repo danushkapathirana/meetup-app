7cab964f0469eaada2c7eff6766a1b57c70e3d60
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-24 16:35:20 +0530

    develop navigate programmatically using useRouter()

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import MeetupDetails from "@/components/meetups/MeetupDetails"
+
+const MeetupDetailsPage = () => {
+    return(
+        <MeetupDetails image="" title="" address="" description="" />
+    )
+}
+
+export default MeetupDetailsPage
