d62e9a5548adca919924573a5419dcf23cc638e0
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-24 23:34:50 +0530

    develop static site generation (SSG)

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,67 @@ const DUMMY_MEETUPS = [
   }
 ]
 
-const HomePage = () => {
+const HomePage = (props) => {
   return(
-    <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={props.meetups} />
   )
 }
 
 export default HomePage
+
+// page pre-rendering with data (static site generation; SSG)
+export const getStaticProps = async () => {
+  return{
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 10
+  }
+}
+
+
+// const HomePage = () => {
+//   const [loadedMeetup, setLoadedMeetup] = useState([])
+
+//   useEffect(() => {
+//     setLoadedMeetup(DUMMY_MEETUPS)
+//   }, [])
+
+//   return(
+//     <MeetupList meetups={loadedMeetup} />
+//   )
+// }
+
+/**
+ * pre-rendering flaw
+ * 
+ * - the page is pre-rendered has basically the snapshot
+ *   after the first component rendering cycle as its content
+ */
+
+/**
+ * consider above commented code
+ * 
+ * - first it will render the component, then it will run useEffect()
+ * - therefore, for the first rendering cycle loadedMeetup is an empty array
+ * 
+ * - after executing useEffect(), it will update useState()
+ * - because of that it will re-render the component again
+ * - therefore, for the second rendering cycle loadedMeetup will be filled
+ * 
+ * not good for SEO; why??? think...
+ */
+
+/**
+ * static generation
+ * 
+ * - when use this mechanism, page component pre-rendered when you build your application
+ * - which means,
+ *   by default, your page is not pre-rendered on the fly on the server, when request reaches the server
+ *   instead it will pre-rendered when you build your application
+ * 
+ * - what about if add more meetups after the build??? think... (hint: outdated data)
+ * 
+ * - to avoid this issue, use another key in the return object (revalidate: )
+ * - it  will re-generate the page AFTER EVERY 10 SECONDS, if there requests
+ */
